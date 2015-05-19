20e843b5d0600ae23f545c4142f198a1633b1574
thejp <jp_55555@hotmail.com>
2015-05-19 22:53:30 +0200

    Some graphical improvements

--- agarMap.js
+++ agarMap.js
@@ -19,23 +19,35 @@ agarMap.ids = [];
 agarMap.init = function init() {
     console.log("starting agarMap...");
     $('body').append($('<div id="mapContainer"></div>')
-                     .css('position', 'absolute')
-                     .css('top', 0)
-                     .css('left', 0)
-                     .css('background-color', 'lightgreen'));
+        .css('position', 'absolute')
+        .css('top', '10px')
+        .css('left', '10px')
+        .css('padding', '15px')
+        .css('border-radius', '5px')
+        .css('background-color', '#32CD32'));
     $('#mapContainer').append($('<div id="map"></div>')
-                              .css('width', '112px')
-                              .css('height', '112px')
-                              .css('border', '2px solid black')
-                              .css('margin', '10px')
-                              .css('background-color', 'white')
-                              .css('box-sizing', 'content-box'));
-    $('#mapContainer').append($('<div id="coords"></div>'));
+        .css('width', '112px')
+        .css('height', '112px')
+        .css('border', '2px solid black')
+        .css('margin-bottom', '10px')
+        .css('background-color', 'white')
+        .css('box-sizing', 'content-box'));
+    $('#mapContainer').append($('<div id="coords"></div>')
+        .css('display', 'none'));
+    $('#mapContainer').append($('<a href="#">details</a>')
+       .click(function(){
+           $('#coords').slideToggle();
+           if($(this).html() === 'details'){
+               $(this).html('hide');
+           } else {
+               $(this).html('details');
+           }
+           return false; //prevent native link behavior
+    }));
 };
 
 agarMap.process = function process(blobs) {
     var text = '';
-    var map = '';
     blobs.forEach(function(blob) {
         if($.inArray(blob.id, agarMap.ids) > -1) {
             var radius = Math.max(blob.size / 100, 1.5);
@@ -43,24 +55,25 @@ agarMap.process = function process(blobs) {
             var top = Math.round((blob.y / 100) - radius);
             var dimension = Math.round(2*radius);
             $('#blob-' + blob.id)
-            .css('width', dimension + 'px')
-            .css('height', dimension + 'px')
-            .css('margin-left', left + 'px')
-            .css('margin-top', top + 'px');
+                .css('width', dimension + 'px')
+                .css('height', dimension + 'px')
+                .css('margin-left', left + 'px')
+                .css('margin-top', top + 'px');
             text += '<div>x: ' + Math.round(blob.x) + ' / y: ' + Math.round(blob.y) + ' (' + Math.round(blob.size) + ')</div>';
         }
     });
-    agarMap.display(text, map);
+    agarMap.display(text);
 };
 
-agarMap.display = function display(text, map) {
+agarMap.display = function display(text) {
     $('#coords').html(text);
 };
 
 agarMap.addBlob = function addBlob(id) {
     $('#map').append($('<div id="blob-' + id + '"></div>')
-                     .css('position', 'absolute')
-                     .css('background-color', 'red'));
+        .css('position', 'absolute')
+        .css('background-color', 'red')
+        .css('border-radius', '50%'));
     agarMap.ids.push(id);
 };
 
@@ -71,16 +84,10 @@ agarMap.removeBlob = function removeBlob(index) {
 
 agarMap.processMessage16 = function processMessage16(dataView) {
     var offset = 1; // Because first byte is message type
-   
+
     // Walk over some data
     var length = dataView.getUint16(offset, true); offset += 2;
     offset += length * 8;
-    
-    /*for(var i = 0; i < length; i++) {
-        offset += 8;
-        var a = dataView.getUint32(offset, true); offset += 4;
-        var b = dataView.getUint32(offset, true); offset += 4;
-    }*/
 
     var blobs = [];
     var existing = [];
@@ -98,8 +105,8 @@ agarMap.processMessage16 = function processMessage16(dataView) {
         blob.color = "#" + new Array(7-blob.color.length).join("0") + blob.color; // Add starting hash and missing 0's
 
         var type = dataView.getUint8(offset++);
-        blob.isVirus = type & 1 === 1;
-        blob.isAgitated = type & 16 === 16;
+        blob.isVirus = (type & 1) === 1;
+        blob.isAgitated = (type & 16) === 16;
 
         if(type & 2) offset += 4; // what is this for?
         if(type & 4) offset += 8;
